54eb6c94ec5862c5c44f8c08b80283d3ab9ef1d9
TheFryyy <47969114+TheFryyy@users.noreply.github.com>
2020-11-27 13:26:45 +0100

    correction of a syntax error

--- server/services/library.js
+++ server/services/library.js
@@ -1,5 +1,5 @@
 const Libraries = require('../models').Library;
 
 const getAll = () => Libraries.findAll();
-const add = (library) => Libraries.creaste(library);
+const add = (library) => Libraries.create(library);
 module.exports = {add, getAll};
