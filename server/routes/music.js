80b7f4be59f249fd27a4bda2213a179d32b03512
julien.lariviere <julien.lariviere@yahoo.fr>
2020-12-03 17:50:21 +0100

    little path correction

--- server/routes/music.js
+++ server/routes/music.js
@@ -9,7 +9,7 @@ router.post('/music/like',authMiddleware.checkAuth , musicController.like)
 router.post('/music',authMiddleware.checkAuth, musicController.createMusic)
 router.post('/music/listen',authMiddleware.checkAuth , musicController.listen)
 
-router.get('/music/:id', authMiddleware.checkAuth, musicController.getFullMusic)
+router.get('/music/full/:id', authMiddleware.checkAuth, musicController.getFullMusic)
 router.get('/music/mostLike',authMiddleware.checkAuth , musicController.mostLike)
 router.get('/music/mostRecent',authMiddleware.checkAuth , musicController.mostRecent)
 router.get('/music/mostFork',authMiddleware.checkAuth , musicController.mostFork)
