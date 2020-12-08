20b4b7a91fb094a11aece764fc680cc100ebaeb0
Xrhoys <yefeng@hotmail.fr>
2020-12-08 18:33:14 +0100

    Cherry pick + error handling

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,8 +4,11 @@ const bcrypt = require('bcrypt');
 
 const authService = require('../services/auth');
 const userService = require('../services/user');
+const { GeneralError, BadRequest } = require('../middlewares/errorClass');
 
-function login(req, res){
+function login(req, res, next){
+     const { login, password } = req.body
+     if (!req.body || !login || !password) return next(new BadRequest('Incomplete credentials.'))
     return authService.authenticate(req.body)
     .then(token => {
          res.send({
@@ -21,7 +24,9 @@ function login(req, res){
     })
 };
 
-function register(req, res){
+function register(req, res, next){
+     const { login, password } = req.body
+     if (!req.body || !login || !password ) return next(new BadRequest('Incomplete credentials.'))
     return userService.getUserByLogin(req.body.login || '')
     .then(exists => {
          if (exists){
@@ -35,8 +40,10 @@ function register(req, res){
               password: bcrypt.hashSync(req.body.password, config.saltRounds)
           }
          return userService.addUser(user)
-         .then(() => res.send({success: true}));
-    });
+         .then(() => res.send({success: true}))
+         .catch(err => next(new GeneralError('Internal Error')));
+    })
+    .catch(err => next(new GeneralError('Internal Error')));
 };
 
 module.exports = {
