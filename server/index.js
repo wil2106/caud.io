724cd45ee2dd578831f92a6580df297d6751ff67
wil2106 <serviantwilliam@gmail.com>
2021-01-14 15:30:06 +0100

    ci test

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const app = require('./App')
 const config = require('./config')
 
 app.listen(process.env.PORT || config.port, () =>
-  console.log('App listening on port ' + config.port)
+  console.log('App listening on port' + config.port)
 )
